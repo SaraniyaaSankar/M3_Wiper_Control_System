bae23f3ac2fcc1bbedc4ceb8b1abd97475e76425
SaraniyaaSankar <101622270+SaraniyaaSankar@users.noreply.github.com>
2022-05-14 20:54:35 +0530

    Update wiper.h

--- 3_Implementation/inc/wiper.h
+++ 3_Implementation/inc/wiper.h
@@ -1,1 +1,21 @@
+#ifndef WIPER_H_INCLUDED
 
+#define WIPER_H_INCLUDED
+
+void off_wiper(void);
+
+void on_wiper(void);
+
+void off_key(void);
+
+void on_key(void);
+
+void Delay_time(void);
+
+void clockwise_motion(void);
+
+void anticlockwise_motion(void);
+
+void Reset(void);
+
+#endif 
